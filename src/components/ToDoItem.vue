9fdfb2393b9cee4cf5602c1c666558e35a741c96
Chris Hodder <26184425+drcsh@users.noreply.github.com>
2021-07-05 11:35:04 +0100

    Playing about with styling

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,7 +1,7 @@
 <template>
-  <li>
-    {{ item.id }}:{{ item.text }}
-    <button @click="handleRemove(item.id)">Remove</button>
+  <li class="item">
+    <span class="item-text">{{ item.text }}</span>
+    <button class="done" @click="handleRemove(item.id)">Done!</button>
   </li>
 </template>
 
@@ -19,5 +19,28 @@ export default {
 </script>
 
 <style scoped>
-
+ .item {
+   padding: 0.2em 0;
+ }
+ .item-text {
+   font-family:'Roboto',sans-serif; 
+ }
+ .done {
+   background-color: #42b983;
+   display:inline-block;
+   padding:0.3em 1.2em;
+   margin-left: 1em;
+   border:0.16em solid rgba(255,255,255,0);
+   border-radius:2em;
+   box-sizing: border-box;
+   text-decoration:none;
+   font-family:'Roboto',sans-serif;
+   font-weight:300;
+   color:#FFFFFF;
+   text-align:center;
+   transition: all 0.2s;
+ }
+ .done:hover {
+   background-color:#4eb5f1;
+ }
 </style>
