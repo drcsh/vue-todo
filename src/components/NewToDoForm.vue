045e446b11e5c73173c096cf5b550b8dbc43a9d3
Chris Hodder <26184425+drcsh@users.noreply.github.com>
2021-07-05 12:21:59 +0100

    Added styling to Add New form.

--- src/components/NewToDoForm.vue
+++ src/components/NewToDoForm.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="new-todo">
-  <form id="new-todo-form" novalidate @submit.prevent="newToDo">
+  <form id="new-todo-form" class="form-inline" novalidate @submit.prevent="newToDo">
     <NewToDoInput v-bind:inputName="formInputName"></NewToDoInput>
-    <button type="submit">Add</button>
+    <button type="submit" class="add">Add</button>
   </form>
 </div>
 
@@ -48,5 +48,28 @@ export default {
 </script>
 
 <style scoped>
-
+  .form-inline {
+    display: inline-flex;
+    flex-flow: row wrap;
+    align-items: center;
+    font-family: Roboto, sans-serif;
+  }
+  .add {
+    background-color: #42b983;
+    display:inline-block;
+    padding:0.3em 1.2em;
+    margin-left: 0.5em;
+    border:0.16em solid rgba(255,255,255,0);
+    border-radius:2em;
+    box-sizing: border-box;
+    text-decoration:none;
+    font-family:'Roboto',sans-serif;
+    font-weight:300;
+    color:#FFFFFF;
+    text-align:center;
+    transition: all 0.2s;
+  }
+  .add:hover {
+    background-color:#4eb5f1;
+  }
 </style>
